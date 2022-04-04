db89fff2773d222612ce8ff3182b0760a87cbd36
Diana R Blanco <54902184+dblan044@users.noreply.github.com>
2022-04-04 19:44:36 -0400

    Update FirstRecurringCharacter.java
    Added instructions

--- Data Structure + Algorithms/Data Structures/Hash Tables/First Recurring Character/FirstRecurringCharacter.java
+++ Data Structure + Algorithms/Data Structures/Hash Tables/First Recurring Character/FirstRecurringCharacter.java
@@ -1,4 +1,8 @@
 import java.util.HashSet;
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]: It should return 2
+//Given an array = [2,1,1,2,3,5,1,2,4]: It should return 1
+//Given an array = [2,3,4,5]: It should return -1
 
 public class FirstRecurringCharacter {
     public Integer firstRecuringChar(int[] array){
